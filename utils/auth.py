b3c8969217e010c11200d2d4d74b44aa4c4cbfd9
Ismel Salazar <ismel.salazar@gmail.com>
2023-06-26 01:10:05 -0400

    Adds decorator "authenticate_user"

--- utils/auth.py
+++ utils/auth.py
@@ -1,6 +1,20 @@
-def getSessionUserId(request):
-    try:
-        user_id = request.session["uid"]
-        return user_id
-    except KeyError:
-        return None
+from django.shortcuts import redirect
+from functools import wraps
+
+
+def get_session_user_id(request):
+  try:
+    user_id = request.session["uid"]
+    return user_id
+  except KeyError:
+    return None
+  
+### Auth decorator
+def authenticate_user(view_function):
+  @wraps(view_function)
+  def wrapper(request, *args, **kwargs):
+    user_id = get_session_user_id(request)
+    if user_id == None:
+      return redirect('root')
+    return view_function(request, user_id, *args, **kwargs)
+  return wrapper

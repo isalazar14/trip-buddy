f7369e965c84ee83f12c74ac6e0b55c0bb3d8c6b
Ismel Salazar <ismel.salazar@gmail.com>
2023-06-27 17:49:54 -0400

    Bugfix: Trip edit page unreachable
    Updates dashboard template to us route names instead of manual urls for link hrefs

--- apps/trip_buddy/urls.py
+++ apps/trip_buddy/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 app_name = 'trips'
 urlpatterns = [
-    path('', views.dashboard, name='dash'),
-    path('create', views.create, name='create'),
-    path('<int:tid>', views.details, name='details'),
-    path('<int:tid>/edit', views.edit, name='edit'),
-    path('<int:tid>/remove', views.remove, name='remove'),
-    path('join', views.join, name='join'),
-    path('leave', views.leave, name='leave'),
+    path('/', views.dashboard, name='dash'),
+    path('/create', views.create, name='create'),
+    path('/<int:tid>', views.details, name='details'),
+    path('/<int:tid>/edit', views.edit, name='edit'),
+    path('/<int:tid>/remove', views.remove, name='remove'),
+    path('/join', views.join, name='join'),
+    path('/leave', views.leave, name='leave'),
     # path('edit', views.edit, name='edit'),
 ]
